a4a47582edf2f52a16c8c1488785faa0dd3bf396
LucasArias <lucaarias@frba.utn.edu.ar>
2024-08-02 11:22:21 -0300

    Pulp fiction sin terminar

---
+++ Parciales/pulpFiction.pl
@@ -0,0 +1,28 @@
+personaje(pumkin,     ladron([licorerias, estacionesDeServicio])).
+personaje(honeyBunny, ladron([licorerias, estacionesDeServicio])).
+personaje(vincent,    mafioso(maton)).
+personaje(jules,      mafioso(maton)).
+personaje(marsellus,  mafioso(capo)).
+personaje(winston,    mafioso(resuelveProblemas)).
+personaje(mia,        actriz([foxForceFive])).
+personaje(butch,      boxeador).
+
+pareja(marsellus, mia).
+pareja(pumkin,    honeyBunny).
+
+%trabajaPara(Empleador, Empleado)
+trabajaPara(marsellus, vincent).
+trabajaPara(marsellus, jules).
+trabajaPara(marsellus, winston).
+
+% Punto 1
+esPeligroso(Personaje):-
+    realizaActPeligrosa(Personaje),
+    tieneEmpleadosPeligrosos(Personaje).
+
+realizaActPeligrosa(Personaje):-
+    personaje(Personaje, mafioso(maton)).
+realizaActPeligrosa(Personaje):-
+    personaje(Personaje, ladron(LocalesRobados)),
+    member(licorerias, LocalesRobados).
+    
